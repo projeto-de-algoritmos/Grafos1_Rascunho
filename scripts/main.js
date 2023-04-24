00e39958951802c96108df8aaa716a8616ed97f9
Lucas <lucas.fn4@gmail.com>
2023-04-24 11:18:23 -0300

    Adds grid creation logic, markup and styles
    Relative to #4.

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,35 @@
-import { GraphAdjList } from "./graphAdjList.js";
+const grid = document.querySelector('.grid');
+/**
+ * Creates a row of a grid.
+ * @returns The new row.
+ */
+function createRow () {
+  const row = document.createElement('div');
+  row.classList.add('grid-row');
+  row.classList.add('flex');
+  return row;
+}
+/**
+ * Creates a column of a grid.
+ * @param row The row in which the column is inserted.
+ */
+function createColumn (row) {
+  const square = document.createElement('div');
+  square.classList.add('square');
+  row.appendChild(square);
+}
+/**
+ * Create a NxN grid.
+ * @param size The grid's number of rows and cols.
+ */
+function createGrid (size) {
+  for(let i = 0; i < size; i++){
+    const row = createRow();
+    for(let j = 0; j < size; j++){
+      createColumn(row);
+    }
+    grid.appendChild(row);
+  }
+}
 
-const testNodes = [
-  { id: 1, name: 'Caio' },
-  { id: 2, name: 'Lucas' },
-];
-
-const graph = new GraphAdjList(testNodes);
-graph.createAdjacency(testNodes[0], testNodes[1]);
-console.log(graph);
-
+createGrid(20);
