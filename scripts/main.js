08cd37c8b2ec756302befd39964825093d94aad9
Lucas <lucas.fn4@gmail.com>
2023-04-25 19:15:50 -0300

    Several changes
    - Change style;
    - Add "rainbow mode";
    - Start Graph logic;

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,9 @@
+import { GraphAdjList } from './graphAdjList.js';
+import { Node } from './node.js';
+
 const grid = document.querySelector('.grid');
+const graph = new GraphAdjList();
+let idCount = 1;
 /**
  * Creates a row of a grid.
  * @returns The new row.
@@ -9,12 +14,19 @@ function createRow () {
   row.classList.add('flex');
   return row;
 }
+
+function getSquareId(square){
+  return +square.id.split('_')[1];
+}
 /**
  * Creates a column of a grid.
  * @param row The row in which the column is inserted.
  */
 function createColumn (row) {
   const square = document.createElement('div');
+  square.setAttribute('id', `sq_${idCount++}`);
+  const squareId = getSquareId(square);
+  graph.addNode(new Node(squareId, true));
   square.classList.add('square');
   square.classList.add('undraggable');
   square.addEventListener('mouseover', changeSquareColor);
@@ -42,10 +54,10 @@ function createGrid (size) {
 let mouseIsBeingHeld = false;
 document.addEventListener('mousedown', () => {
   mouseIsBeingHeld = true;
-})
+});
 document.addEventListener('mouseup', ()=> {
   mouseIsBeingHeld = false;
-})
+});
 /**
  * Checks if mouse is being moved and held down.
  * @param event The event that triggers the function.
@@ -56,11 +68,21 @@ function mouseIsMovingAndHeldDown (event) {
   }
 }
 /**
+ * Get a random integer between 
+ * 0 and 255.
+ */
+function getRandomNumber(){ 
+  return Math.floor(Math.random() * 1000) % 256;
+}
+/**
  * Changes the square color.
  * @param event The event that triggers the function.
  */
 function changeSquareColor (event){
   if( !mouseIsMovingAndHeldDown(event) ) return;
-  event.target.style.backgroundColor = 'black';
+  const rgb = `${getRandomNumber()},${getRandomNumber()},${getRandomNumber()}`;
+  event.target.style.backgroundColor = `rgb(${rgb})`;
+  const id = getSquareId(event.target);
 }
+
 createGrid(100);
---
+++ scripts/node.js
@@ -0,0 +1,9 @@
+/**
+ * A Node of a Graph.
+ */
+export class Node {
+  constructor(id, fill){
+    this.id = id;
+    this.fill = fill;
+  }
+}
