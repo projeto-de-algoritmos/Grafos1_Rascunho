0e02b5587fb794ec6a5b677d810b368ea12ad01d
Lucas <lucas.fn4@gmail.com>
2023-04-25 23:54:58 -0300

    Format document and add logic for creating adjacencies

--- scripts/main.js
+++ scripts/main.js
@@ -1,19 +1,26 @@
-import { GraphAdjList } from './graphAdjList.js';
-import { Node } from './node.js';
-import { COLOR_BLACK, COLOR_WHITE, GRID_SIZE, getRandomNumber }from './utils.js';
+import { GraphAdjList } from "./graphAdjList.js";
+import { Node } from "./node.js";
+import {
+  COLOR_BLACK,
+  COLOR_WHITE,
+  GRID_SIZE,
+  getRandomNumber,
+} from "./utils.js";
 
-const grid = document.querySelector('.grid');
+const grid = document.querySelector(".grid");
 const graph = new GraphAdjList();
 let idCount = 1;
+let rowCount = 1;
+let children = [];
 /**
  * All the possible modes that the user can select
  * to change the grid.
  */
 const modes = {
-  color: 'color',
-  rainbow: 'rainbow',
-  eraser: 'eraser',
-  fill: 'fill',
+  color: "color",
+  rainbow: "rainbow",
+  eraser: "eraser",
+  fill: "fill",
 };
 /** The `mode` variable controls the possible modes
  * that the user can select to change the grid.
@@ -23,32 +30,83 @@ let mode = modes.color;
  * Creates a row of a grid.
  * @returns The new row.
  */
-function createRow () {
-  const row = document.createElement('div');
-  row.classList.add('grid-row');
-  row.classList.add('flex');
+function createRow() {
+  const row = document.createElement("div");
+  row.classList.add("grid-row");
+  row.classList.add("flex");
   return row;
 }
 /**
  * Get a square's identifier.
  * @param square The square of interest.
  */
-function getSquareId(square){
-  return +square.id.split('_')[1];
+function getSquareId(square) {
+  return +square.id.split("_")[1];
 }
+/**
+ * Get a Square(which is also a Node of a Graph)
+ * by its id.
+ * @param id The identifier of the Square.
+ * @returns The Square.
+ */
+function getSquareById(id) {
+  return document.getElementById(`sq_${id}`);
+}
+
+function createAdjacencies(rootSquare) {
+  const rootNodeId = getSquareId(rootSquare);
+  const rootNode = new Node(rootNodeId, rootSquare.style.backgroundColor);
+
+  // upper adjacency
+  const upperAdjacencyId = rootNodeId - GRID_SIZE;
+  if (upperAdjacencyId > 0) {
+    const upperAdjacentSquare = getSquareById(upperAdjacencyId);
+    const upperNode = new Node(
+      upperAdjacencyId,
+      upperAdjacentSquare.style.backgroundColor
+    );
+    graph.createAdjacency(rootNode, upperNode);
+  }
+
+  // rightmost adjacency
+  const rightmostAdjacencyId = rootNodeId + 1;
+  const rightmostAdjacentSquare = getSquareById(rightmostAdjacencyId);
+  if (
+    rightmostAdjacencyId <= GRID_SIZE * GRID_SIZE &&
+    +rightmostAdjacentSquare.getAttribute("row") ===
+      +rootSquare.getAttribute("row")
+  ) {
+    const rightNode = new Node(
+      rightmostAdjacencyId,
+      rightmostAdjacentSquare.style.backgroundColor
+    );
+    graph.createAdjacency(rootNode, rightNode);
+  }
+}
+
 /**
  * Creates a column of a grid.
  * @param row The row in which the column is inserted.
  */
-function createColumn (row) {
-  const square = document.createElement('div');
-  square.setAttribute('id', `sq_${idCount++}`);
+function createColumn(row) {
+  const square = document.createElement("div");
+
+  square.setAttribute("id", `sq_${idCount++}`);
+  square.setAttribute("row", `${rowCount}`);
+
+  square.style.backgroundColor = COLOR_WHITE;
+
+  square.classList.add("square");
+  square.classList.add("undraggable");
+
+  square.addEventListener("mouseover", updateGrid);
+  square.addEventListener("click", updateGrid);
+
   const squareId = getSquareId(square);
-  graph.addNode(new Node(squareId, COLOR_WHITE));
-  square.classList.add('square');
-  square.classList.add('undraggable');
-  square.addEventListener('mouseover', updateGrid);
-  square.addEventListener('click', updateGrid);
+  const node = new Node(squareId, COLOR_WHITE);
+  graph.addNode(node);
+
+  children.push(square);
 
   row.appendChild(square);
 }
@@ -56,34 +114,36 @@ function createColumn (row) {
  * Create a NxN grid.
  * @param size The grid's number of rows and cols.
  */
-function createGrid (size) {
-  for(let i = 0; i < size; i++){
+function createGrid(size) {
+  for (let i = 0; i < size; i++) {
     const row = createRow();
-    for(let j = 0; j < size; j++){
+    for (let j = 0; j < size; j++) {
       createColumn(row);
     }
     grid.appendChild(row);
+    rowCount++;
   }
+  children.forEach((child) => createAdjacencies(child));
 }
 /**
- * The following events must be listened to 
+ * The following events must be listened to
  * in order to control if the grid is only being
  * drawn into if the user is moving the mouse while
  * holding it down.
  */
 let mouseIsBeingHeld = false;
-document.addEventListener('mousedown', () => {
+document.addEventListener("mousedown", () => {
   mouseIsBeingHeld = true;
 });
-document.addEventListener('mouseup', ()=> {
+document.addEventListener("mouseup", () => {
   mouseIsBeingHeld = false;
 });
 /**
  * Checks if mouse is being moved and held down.
  * @param event The event that triggers the function.
  */
-function mouseIsMovingAndHeldDown (event) {
-  if(event.type === 'mouseover' && mouseIsBeingHeld) {
+function mouseIsMovingAndHeldDown(event) {
+  if (event.type === "mouseover" && mouseIsBeingHeld) {
     return true;
   }
 }
@@ -93,27 +153,27 @@ function mouseIsMovingAndHeldDown (event) {
  * @param event The event that triggers the function.
  * @param mode The mode of painting.
  */
-function updateGrid (event) {
+function updateGrid(event) {
   const squareId = getSquareId(event.target);
-  if(event.type === 'click' && mode === modes.fill){
+  if (event.type === "click" && mode === modes.fill) {
     const rootNode = graph.nodes[squareId];
     const squareColor = rootNode.color;
     return graph.breadthFirstSearch(rootNode, squareColor);
   }
-  if(event.type === 'click') {
+  if (event.type === "click") {
     paintSquare(event.target, COLOR_BLACK);
     graph.updateNodeColor(squareId, COLOR_BLACK);
     return;
   }
-  if( !mouseIsMovingAndHeldDown(event) ) return;
-  if(mode === modes.color){
+  if (!mouseIsMovingAndHeldDown(event)) return;
+  if (mode === modes.color) {
     paintSquare(event.target, COLOR_BLACK);
   }
-  if(mode === modes.rainbow){
+  if (mode === modes.rainbow) {
     const rgb = `rgb(${getRandomNumber()},${getRandomNumber()},${getRandomNumber()})`;
     paintSquare(event.target, rgb);
   }
-  if(mode === modes.eraser){
+  if (mode === modes.eraser) {
     paintSquare(event.target, COLOR_WHITE);
   }
   graph.updateNodeColor(squareId, event.target.style.backgroundColor);
@@ -129,39 +189,39 @@ function paintSquare(square, color) {
 
 createGrid(GRID_SIZE);
 
-const rainbowButton = document.getElementById('rainbow');
-const showGridButton = document.getElementById('show-grid');
-const fillButton = document.getElementById('fill');
-const colorButton = document.getElementById('color');
-const eraserButton = document.getElementById('eraser');
-const eraseAllButton = document.getElementById('erase-all');
+const rainbowButton = document.getElementById("rainbow");
+const showGridButton = document.getElementById("show-grid");
+const fillButton = document.getElementById("fill");
+const colorButton = document.getElementById("color");
+const eraserButton = document.getElementById("eraser");
+const eraseAllButton = document.getElementById("erase-all");
 
-rainbowButton.addEventListener('click', () => {
+rainbowButton.addEventListener("click", () => {
   mode = modes.rainbow;
 });
 
-colorButton.addEventListener('click', () => {
+colorButton.addEventListener("click", () => {
   mode = modes.color;
 });
 
-fillButton.addEventListener('click', () => {
+fillButton.addEventListener("click", () => {
   mode = modes.fill;
 });
 
-eraserButton.addEventListener('click', () => {
+eraserButton.addEventListener("click", () => {
   mode = modes.eraser;
 });
 
-eraseAllButton.addEventListener('click', () => {
-  const squares = document.getElementsByClassName('square');
-  [...squares].forEach(square=>{
+eraseAllButton.addEventListener("click", () => {
+  const squares = document.getElementsByClassName("square");
+  [...squares].forEach((square) => {
     paintSquare(square, COLOR_WHITE);
-  }); 
+  });
 });
 
-showGridButton.addEventListener('click', () => {
-  const squares = document.getElementsByClassName('square');
-  [...squares].forEach(square=>{
-    square.classList.toggle('show-tracks');
+showGridButton.addEventListener("click", () => {
+  const squares = document.getElementsByClassName("square");
+  [...squares].forEach((square) => {
+    square.classList.toggle("show-tracks");
   });
 });
