76fe2e080f03df7045feb8998e52bb95335707d9
Lucas <lucas.fn4@gmail.com>
2023-04-28 17:00:14 -0300

    Add message that shows that the file has been downloaded.

--- scripts/export.js
+++ scripts/export.js
@@ -1,13 +1,15 @@
 const exportButton = document.getElementById("export");
 exportButton.addEventListener("click", exportGrid);
 
-function exportGrid() {
+async function exportGrid() {
     const grid = document.getElementById("grid");
   
-    html2canvas(grid).then(function(canvas) {
+    await html2canvas(grid).then(function(canvas) {
       const link = document.createElement("a");
       link.download = "rascunho.png";
       link.href = canvas.toDataURL("image/png");
       link.click();
     });
+
+    alert('Download do rascunho completo!');
   }
