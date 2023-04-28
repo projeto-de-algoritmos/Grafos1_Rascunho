4b181bd6acdd0730d11d9d93a1ad277a46defff6
Lucas <lucas.fn4@gmail.com>
2023-04-28 17:08:14 -0300

    Adds confirmation message

--- scripts/export.js
+++ scripts/export.js
@@ -3,7 +3,7 @@ exportButton.addEventListener("click", exportGrid);
 
 async function exportGrid() {
     const grid = document.getElementById("grid");
-  
+    if(!confirm('Você deseja baixar o rascunho?')) return;
     await html2canvas(grid).then(function(canvas) {
       const link = document.createElement("a");
       link.download = "rascunho.png";
