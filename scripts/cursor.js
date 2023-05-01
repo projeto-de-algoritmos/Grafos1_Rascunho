18aa33bf736527d16f1022be1fad86c5e68e8ea5
oCaioOliveira <caiocesar31951910@hotmail.com>
2023-05-01 15:01:00 -0300

    create logic to select buttons

--- scripts/cursor.js
+++ scripts/cursor.js
@@ -7,16 +7,16 @@ grid.addEventListener("mousemove", (e) => {
   const mode = getMode();
   switch (mode) {
     case "color":
-        cursorIcon.style.backgroundImage = "url('../icons/brush.svg')";
+        cursorIcon.style.backgroundImage = "url('./../icons/brush.svg')";
         break;
     case "fill":
-        cursorIcon.style.backgroundImage = "url('../icons/fill.png')";
+        cursorIcon.style.backgroundImage = "url('./../icons/fill.png')";
         break;
     case "eraser":
-        cursorIcon.style.backgroundImage = "url('../icons/eraser.svg')";
+        cursorIcon.style.backgroundImage = "url('./../icons/eraser.svg')";
         break;
     case "rainbow":
-        cursorIcon.style.backgroundImage = "url('../icons/rainbow.png')";
+        cursorIcon.style.backgroundImage = "url('./../icons/rainbow.png')";
         break;
     default:
       cursorIcon.style.backgroundImage = "none";
@@ -26,4 +26,64 @@ grid.addEventListener("mousemove", (e) => {
     cursorIcon.style.left = `${e.pageX}px`;
     cursorIcon.style.top = `${e.pageY}px`;
   }
+});
+
+let timeoutActive = false;
+let gridActive = false;
+
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach(button => {
+  if (button.id === "small-brush" || button.id === "medium-brush" || button.id === "big-brush") {
+    const brushButton = document.getElementById("brush");
+    button.addEventListener("click", () => {
+      buttons.forEach(b => {
+        if (b.id !== "timeout" && b.id !== "show-grid") {
+          b.classList.remove("selected");
+        }
+      });
+      brushButton.classList.add("selected");
+      button.classList.add("selected");
+    });
+  } else if (button.id === "small-eraser" || button.id === "medium-eraser" || button.id === "big-eraser") {
+    const brushButton = document.getElementById("eraser");
+    button.addEventListener("click", () => {
+      buttons.forEach(b => {
+        if (b.id !== "timeout" && b.id !== "show-grid") {
+          b.classList.remove("selected");
+        }
+      });
+      brushButton.classList.add("selected");
+      button.classList.add("selected");
+    });
+  } else if (button.id === "timeout") {
+    button.addEventListener("click", () => {
+      if (timeoutActive) {
+        button.classList.remove("selected");
+        timeoutActive = false;
+      } else {
+        button.classList.add("selected");
+        timeoutActive = true;
+      }
+    });
+  } else if (button.id === "show-grid") {
+    button.addEventListener("click", () => {
+      if (gridActive) {
+        button.classList.remove("selected");
+        gridActive = false;
+      } else {
+        button.classList.add("selected");
+        gridActive = true;
+      }
+    });
+  } else if (button.id !== "undo" && button.id !== "do" && button.id !== "export" && button.id !== "erase-all") {
+    button.addEventListener("click", () => {
+      buttons.forEach(b => {
+        if (b.id !== "timeout" && b.id !== "show-grid") {
+          b.classList.remove("selected");
+        }
+      });
+      button.classList.add("selected");
+    });
+  } 
 });
